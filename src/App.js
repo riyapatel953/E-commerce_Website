dc77ed502157ab8ecc346294dfb2eb1c69f67c03
unknown <Riya Patel>
2022-06-24 08:07:48 +0930

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import Products from './component/Products';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <Products/>
+    </>
+  );
+}
+
+export default App;
